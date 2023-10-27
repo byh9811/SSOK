5df8f0c463a282dd06c283679a9ccf8878fd22d6
apicad0307 <apicad0307@naver.com>
2023-10-27 09:05:50 +0900

    init: namecard 프로젝트 세팅

---
+++ namecard/src/test/java/com/ssok/namecard/NamecardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssok.namecard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamecardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

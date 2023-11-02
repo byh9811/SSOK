5db3897a4ecc5a276cef3ce37ed8b2c635a68a8c
eat159357 <eat159357@gmail.com>
2023-11-02 16:08:55 +0900

    S09P31C107-483/test: MongoDB연결 테스트

--- pocket/src/main/java/com/ssok/base/BaseApplication.java
+++ pocket/src/main/java/com/ssok/base/BaseApplication.java
@@ -2,9 +2,11 @@ package com.ssok.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BaseApplication {
 
 	public static void main(String[] args) {

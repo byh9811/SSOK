0ee9ff8fabf997b5911790995fe6284b91530b33
Yuhojae <ho_0214@naver.com>
2023-11-03 22:12:08 +0900

    init: idcard초기 프로젝트 생성

---
+++ idcard/src/main/java/com/ssok/idcard/IdcardApplication.java
@@ -0,0 +1,13 @@
+package com.ssok.idcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdcardApplication.class, args);
+	}
+
+}

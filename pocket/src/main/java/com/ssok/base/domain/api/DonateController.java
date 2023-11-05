cf12277b43f83948c91a6c4c588598ebf4975f0d
eat159357 <eat159357@gmail.com>
2023-11-05 19:53:28 +0900

    S09P31C107-496/feat: 기부 컨트롤러 생성

---
+++ pocket/src/main/java/com/ssok/base/domain/api/DonateController.java
@@ -0,0 +1,14 @@
+package com.ssok.base.domain.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/donate-service")
+public class DonateController {
+
+}

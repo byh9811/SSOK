3ef79c4619c86ecce062b7558fef47fc1575892f
apicad0307 <apicad0307@naver.com>
2023-11-05 21:52:40 +0900

    fix: enablefeignclient 수정

--- namecard/src/main/java/com/ssok/namecard/client/MemberServiceClient.java
+++ namecard/src/main/java/com/ssok/namecard/client/MemberServiceClient.java
@@ -1,6 +1,5 @@
 package com.ssok.namecard.client;
 
-import com.ssok.namecard.client.config.FeignConfig;
 import com.ssok.namecard.global.api.ApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Component
-@FeignClient(name = "member-service", url = "https://member.ssok.site", configuration = FeignConfig.class)
+@FeignClient(name = "efkenf")
 public interface MemberServiceClient {
 
     @GetMapping(produces = "application/json", value = "/member/seq")

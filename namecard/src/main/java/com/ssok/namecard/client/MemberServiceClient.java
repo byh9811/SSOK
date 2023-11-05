ddf420663aea05bd7bea15338b84e5fb4faf6538
apicad0307 <apicad0307@naver.com>
2023-11-05 21:55:32 +0900

    fix: Feign 이름 수정

--- namecard/src/main/java/com/ssok/namecard/client/MemberServiceClient.java
+++ namecard/src/main/java/com/ssok/namecard/client/MemberServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Component
-@FeignClient(name = "efkenf")
+@FeignClient(name = "member-service")
 public interface MemberServiceClient {
 
     @GetMapping(produces = "application/json", value = "/member/seq")

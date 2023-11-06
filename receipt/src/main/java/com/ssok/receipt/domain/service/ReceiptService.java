7e616c84d017603c82b02a1bae205b5419d22330
배용현 <byh9811@naver.com>
2023-11-07 07:21:07 +0900

    S09P31C107-682/feat: 탄소중립포인트 검사 로직 생성

--- receipt/src/main/java/com/ssok/receipt/domain/service/ReceiptService.java
+++ receipt/src/main/java/com/ssok/receipt/domain/service/ReceiptService.java
@@ -6,6 +6,7 @@ import com.ssok.receipt.domain.maria.entity.Receipt;
 import com.ssok.receipt.domain.maria.repository.PurchaseItemRepository;
 import com.ssok.receipt.domain.maria.repository.ReceiptRepository;
 import com.ssok.receipt.domain.service.dto.ReceiptCreateServiceDto;
+import com.ssok.receipt.global.openfeign.member.MemberClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,18 +24,30 @@ public class ReceiptService {
     private final ReceiptEventHandler eventHandler;
     private final ReceiptRepository receiptRepository;
     private final PurchaseItemRepository purchaseItemRepository;
+    private final MemberClient memberClient;
 
     public void createReceipt(ReceiptCreateServiceDto receiptCreateServiceDto) {
         Receipt receipt = Receipt.fromCreateDto(receiptCreateServiceDto);
         List<InnerPaymentItem> paymentItemList = receiptCreateServiceDto.paymentItemList();
         List<PurchaseItem> purchaseItemList = new ArrayList<>();
+        List<String> carbonNeutralItemList = new ArrayList<>();     // DB or Enum에서 읽기
         for (InnerPaymentItem innerPaymentItem : paymentItemList) {
-            purchaseItemList.add(PurchaseItem.from(receipt, innerPaymentItem));
+            boolean isCNI = false;
+            for (String carbonNeutralItem : carbonNeutralItemList) {
+                if (carbonNeutralItem.equals(innerPaymentItem.itemName())) {      // 이름으로 비교
+                    isCNI = true;
+                    break;
+                }
+            }
+
+            purchaseItemList.add(PurchaseItem.from(receipt, innerPaymentItem, isCNI));
         }
 
         purchaseItemRepository.saveAll(purchaseItemList);
         receiptRepository.save(receipt);
         eventHandler.createReceipt(receiptCreateServiceDto);
+
+
     }
 
 }

730ed05cd47f6c6dbfdf2981d5093e79d6ce4cbf
나종현 <i0364842@naver.com>
2023-11-10 11:44:50 +0900

    feat: 명함 받기 시 리스트로 목록 받기

--- ssok/lib/screens/businesscard/business_card_send_bluetooth_page.dart
+++ ssok/lib/screens/businesscard/business_card_send_bluetooth_page.dart
@@ -1,24 +1,20 @@
-import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:nearby_connections/nearby_connections.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:ssok/http/http.dart';
-import 'package:ssok/http/token_manager.dart';
 
-class BusinessCardSendBlueToothPage extends StatefulWidget {
-  const BusinessCardSendBlueToothPage({super.key});
+class BusinessCardSendBluetoothPage extends StatefulWidget {
+  const BusinessCardSendBluetoothPage({super.key});
 
   @override
-  State<BusinessCardSendBlueToothPage> createState() =>
-      _BusinessCardSendBlueToothPageState();
+  State<BusinessCardSendBluetoothPage> createState() =>
+      _BusinessCardSendBluetoothPageState();
 }
 
-class _BusinessCardSendBlueToothPageState
-    extends State<BusinessCardSendBlueToothPage> {
-  ApiService apiService = ApiService();
+class _BusinessCardSendBluetoothPageState
+    extends State<BusinessCardSendBluetoothPage> {
   final String userName = Random().nextInt(10000).toString();
   final Strategy strategy = Strategy.P2P_STAR;
   Map<String, ConnectionInfo> endpointMap = {};
@@ -26,73 +22,127 @@ class _BusinessCardSendBlueToothPageState
   Map<int, String> map = {};
   bool advertising = false;
   bool scanning = false;
-  late int namecardSeq;
 
-  void scanningStart() async {
+  @override
+  void initState() {
+    super.initState();
+    [
+      Permission.bluetooth,
+      Permission.bluetoothAdvertise,
+      Permission.bluetoothConnect,
+      Permission.bluetoothScan
+    ].request();
+  }
+
+  void advertisingStart() async {
     try {
-      bool a = await Nearby().startDiscovery(
+      bool a = await Nearby().startAdvertising(
         userName,
         strategy,
-        onEndpointFound: (id, name, serviceId) {
-          // show sheet automatically to request connection
-          showModalBottomSheet(
-            context: context,
-            builder: (builder) {
-              return Center(
-                child: Column(
-                  children: <Widget>[
-                    Text("id: $id"),
-                    Text("Name: $name"),
-                    Text("ServiceId: $serviceId"),
-                    ElevatedButton(
-                      child: const Text("Request Connection"),
-                      onPressed: () {
-                        Navigator.pop(context);
-                        Nearby().requestConnection(
-                          userName,
-                          id,
-                          onConnectionInitiated: (id, info) {
-                            onConnectionInit(id, info);
-                          },
-                          onConnectionResult: (id, status) {
-                            showSnackbar(status);
-                          },
-                          onDisconnected: (id) {
-                            setState(() {
-                              endpointMap.remove(id);
-                            });
-                            showSnackbar(
-                                "Disconnected from: ${endpointMap[id]!.endpointName}, id $id");
-                          },
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
+        onConnectionInitiated: onConnectionInit,
+        onConnectionResult: (id, status) {
+          showSnackbar(status);
         },
-        onEndpointLost: (id) {
+        onDisconnected: (id) {
           showSnackbar(
-              "Lost discovered Endpoint: ${endpointMap[id]?.endpointName}, id $id");
+              "Disconnected: ${endpointMap[id]!.endpointName}, id $id");
+          setState(() {
+            endpointMap.remove(id);
+          });
         },
       );
-      showSnackbar("DISCOVERING: $a");
+      showSnackbar("어필: $a");
       setState(() {
-        scanning = true;
+        advertising = true;
       });
-    } catch (e) {
-      showSnackbar(e);
+      Future.delayed(const Duration(milliseconds: 10000), () async {
+        await Nearby().stopAdvertising();
+        // pointClear();
+        setState(() {
+          advertising = false;
+        });
+      });
+    } catch (exception) {
+      showSnackbar(exception);
     }
-    Future.delayed(const Duration(milliseconds: 10000), () async {
-      await Nearby().stopDiscovery();
-      pointClear();
-      setState(() {
-        scanning = false;
-      });
+  }
+
+  void sendBusinessCard(int myNamecardSeq) {
+    endpointMap.forEach((key, value) {
+      String myNamecardSeqString = myNamecardSeq.toString();
+
+      showSnackbar(
+          " ${value.endpointName} $myNamecardSeqString이 ~에게 명함을 보내요 ~~ , id: $key");
+      Nearby().sendBytesPayload(
+          key, Uint8List.fromList(myNamecardSeqString.codeUnits));
     });
   }
+
+  // void scanningStart() async {
+  //   try {
+  //     bool a = await Nearby().startDiscovery(
+  //       userName,
+  //       strategy,
+  //       onEndpointFound: (id, name, serviceId) {
+  //         // show sheet automatically to request connection
+  //         showModalBottomSheet(
+  //           context: context,
+  //           builder: (builder) {
+  //             return Center(
+  //               child: Column(
+  //                 children: <Widget>[
+  //                   Text("id: $id"),
+  //                   Text("Name: $name"),
+  //                   Text("ServiceId: $serviceId"),
+  //                   ElevatedButton(
+  //                     child: const Text("Request Connection"),
+  //                     onPressed: () {
+  //                       Navigator.pop(context);
+  //                       Nearby().requestConnection(
+  //                         userName,
+  //                         id,
+  //                         onConnectionInitiated: (id, info) {
+  //                           onConnectionInit(id, info);
+  //                         },
+  //                         onConnectionResult: (id, status) {
+  //                           showSnackbar(status);
+  //                         },
+  //                         onDisconnected: (id) {
+  //                           setState(() {
+  //                             endpointMap.remove(id);
+  //                           });
+  //                           showSnackbar(
+  //                               "Disconnected from: ${endpointMap[id]!.endpointName}, id $id");
+  //                         },
+  //                       );
+  //                     },
+  //                   ),
+  //                 ],
+  //               ),
+  //             );
+  //           },
+  //         );
+  //       },
+  //       onEndpointLost: (id) {
+  //         showSnackbar(
+  //             "Lost discovered Endpoint: ${endpointMap[id]?.endpointName}, id $id");
+  //       },
+  //     );
+  //     showSnackbar("DISCOVERING: $a");
+  //     setState(() {
+  //       scanning = true;
+  //     });
+  //   } catch (e) {
+  //     showSnackbar(e);
+  //   }
+  //   Future.delayed(const Duration(milliseconds: 10000), () async {
+  //     await Nearby().stopDiscovery();
+  //     pointClear();
+  //     setState(() {
+  //       scanning = false;
+  //     });
+  //   });
+  // }
 
   void pointClear() async {
     await Nearby().stopAllEndpoints();
@@ -101,40 +151,18 @@ class _BusinessCardSendBlueToothPageState
     });
   }
 
-  void transfer(int namecardASeq, int namecardBSeq) async {
-    print("전송 시작!!!");
-    final response = await apiService.postRequest(
-        'namecard-service/exchange/single',
-        {
-          "namecardASeq": "$namecardASeq",
-          "namecardBSeq": "$namecardBSeq",
-          "lat": "35.193844",
-          "lon": "126.8102029"
-        },
-        TokenManager().accessToken);
-    print("전송 끝!!!");
-    print(jsonDecode(utf8.decode(response.bodyBytes)));
-    if (response.statusCode == 200) {
-      final jsonData = jsonDecode(response.body);
-      print("전송 성공!!!");
-    } else {
-      print("전송 실패!!!");
-      throw Exception('Failed to load');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    namecardSeq = ModalRoute.of(context)!.settings.arguments as int;
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+    int namecardSeq = ModalRoute.of(context)!.settings.arguments as int;
     return Scaffold(
       backgroundColor: Colors.transparent,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
         title: Text(
-          "명함 받기",
+          "명함 교환",
           style: TextStyle(
             fontSize: 19,
             color: Colors.white,
@@ -178,25 +206,30 @@ class _BusinessCardSendBlueToothPageState
           ElevatedButton(
             onPressed: () {
               pointClear();
-              scanningStart();
+              advertisingStart();
+              // scanningStart();
             },
-            child: Text(scanning ? "스캔 중" : "스캔 시작"),
+            child: Text(advertising ? "어필 중" : "어필시작"),
           ),
+          SizedBox(
+            height: screenHeight * 0.02,
+          ),
+          // ElevatedButton(
+          //   onPressed: () {
+          //     pointClear();
+          //     scanningStart();
+          //   },
+          //   child: Text(scanning ? "스캔 중" : "스캔 시작"),
+          // ),
           SizedBox(
             height: screenHeight * 0.025,
           ),
-          // ElevatedButton(
-          //   child: const Text("명함 전송"),
-          //   onPressed: () async {
-          //     endpointMap.forEach((key, value) {
-          //       String a = Random().nextInt(100).toString();
-
-          //       showSnackbar(
-          //           " ${value.endpointName} 에게 $a 라는 데이터 보내요 ~~ , id: $key");
-          //       Nearby().sendBytesPayload(key, Uint8List.fromList(a.codeUnits));
-          //     });
-          //   },
-          // ),
+          ElevatedButton(
+            child: const Text("명함 전송"),
+            onPressed: () async {
+              sendBusinessCard(namecardSeq);
+            },
+          ),
         ],
       ),
     );
@@ -235,34 +268,25 @@ class _BusinessCardSendBlueToothPageState
                         String str = String.fromCharCodes(
                             payload.bytes!); // 바이트 데이터를 문자열로 반환
                         showSnackbar("$endid: $str");
-                        int seq = int.parse(str);
-                        print(
-                            "namecardSeqA :  $namecardSeq , namecardSeqB : $seq");
-
-                        transfer(namecardSeq, seq);
                       }
                     },
-                    // onPayloadTransferUpdate: (endid, payloadTransferUpdate) {
-                    //   if (payloadTransferUpdate
-                    //           .status == // 상태 == IN_PROGRESS인 경우 전송 중인 데이터 양 등을 업데이트
-                    //       PayloadStatus.IN_PROGRESS) {
-                    //     print(
-                    //         "${payloadTransferUpdate.bytesTransferred} 여긴가??");
-                    //   } else if (payloadTransferUpdate.status ==
-                    //       PayloadStatus.FAILURE) {
-                    //     // 상태 == FAILURE인 경우  전송 실패에 대한 처리를 수행
-                    //     print("failed");
-                    //     showSnackbar("$endid: FAILED to transfer file");
-                    //   } else if (payloadTransferUpdate
-                    //           .status == // 상태 == SUCCESS 전송이 성공적으로 완료되었을 때 처리를 수행
-                    //       PayloadStatus.SUCCESS) {
-                    //     // transfer(namecardSeq, payloadTransferUpdate.totalBytes);
-                    //     print("ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ");
-                    //     print(payloadTransferUpdate.totalBytes);
-                    //     showSnackbar(
-                    //         "$endid  ${payloadTransferUpdate.totalBytes} 명함 받기에 성공했어요! 너도 보낼래?");
-                    //   }
-                    // },
+                    onPayloadTransferUpdate: (endid, payloadTransferUpdate) {
+                      if (payloadTransferUpdate
+                              .status == // 상태 == IN_PROGRESS인 경우 전송 중인 데이터 양 등을 업데이트
+                          PayloadStatus.IN_PROGRESS) {
+                        print(payloadTransferUpdate.bytesTransferred);
+                      } else if (payloadTransferUpdate.status ==
+                          PayloadStatus.FAILURE) {
+                        // 상태 == FAILURE인 경우  전송 실패에 대한 처리를 수행
+                        print("failed");
+                        showSnackbar("$endid: FAILED to transfer file");
+                      } else if (payloadTransferUpdate
+                              .status == // 상태 == SUCCESS 전송이 성공적으로 완료되었을 때 처리를 수행
+                          PayloadStatus.SUCCESS) {
+                        showSnackbar(
+                            "$endid success, total bytes = ${payloadTransferUpdate.totalBytes}");
+                      }
+                    },
                   );
                 },
               ),

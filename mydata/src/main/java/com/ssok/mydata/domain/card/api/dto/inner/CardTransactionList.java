8a78fe510f46305e05dd6c658d33bb224e38130f
배용현 <byh9811@naver.com>
2023-11-10 10:11:50 +0900

    S09P31C107-682/chore: 데이터 타입 변경

--- mydata/src/main/java/com/ssok/mydata/domain/card/api/dto/inner/CardTransactionList.java
+++ mydata/src/main/java/com/ssok/mydata/domain/card/api/dto/inner/CardTransactionList.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -17,7 +17,7 @@ public class CardTransactionList {
         private String approvedNum;
 
         @JsonProperty("approved_dtime")
-        private Date approvedDtime;
+        private LocalDateTime approvedDtime;
 
         @JsonProperty("status")
         private String status;
@@ -26,7 +26,7 @@ public class CardTransactionList {
         private String payType;
 
         @JsonProperty("trans_dtime")
-        private Date transDtime;
+        private LocalDateTime transDtime;
 
         @JsonProperty("merchant_name")
         private String merchantName;

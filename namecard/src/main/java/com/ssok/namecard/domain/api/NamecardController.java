1325b625f0303c6e076dbfc60c144aa041979dda
apicad0307 <apicad0307@naver.com>
2023-11-11 21:41:05 +0900

    fix: 등록 중 postmapping 어노테이션 변경

--- namecard/src/main/java/com/ssok/namecard/domain/api/NamecardController.java
+++ namecard/src/main/java/com/ssok/namecard/domain/api/NamecardController.java
@@ -14,6 +14,7 @@ import com.ssok.namecard.global.api.ApiResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,8 +37,9 @@ public class NamecardController {
 
     /**
      * 명함 등록
+     * @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
      */
-    @PostMapping("/")
+    @PostMapping(path = "/", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ApiResponse<Long> createNamecardRequest(
         @RequestHeader(name = "MEMBER-UUID") String memberUuid,
         @RequestPart NamecardCreateRequest namecardCreateRequest,

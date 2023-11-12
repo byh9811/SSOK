cde567a248fcc4e5644e319272dd22f30f00d17a
Yuhojae <ho_0214@naver.com>
2023-11-12 22:01:51 +0900

    fix : ocr api 에러처리(신분증, 면허증, 명함)

--- idcard/src/main/java/com/ssok/idcard/domain/api/response/RecognizedLicenseResponse.java
+++ idcard/src/main/java/com/ssok/idcard/domain/api/response/RecognizedLicenseResponse.java
@@ -24,17 +24,17 @@ public record RecognizedLicenseResponse(
     public static RecognizedLicenseResponse from(LicenseOcrResponse.DriverLicense license) {
 
         return RecognizedLicenseResponse.builder()
-                .licenseName(license.getName().get(0).getFormatted().getValue())
-                .licensePersonalNumber(license.getPersonalNum().get(0).getFormatted().getValue())
-                .licenseType(license.getType().get(0).getText())
-                .licenseAddress(license.getAddress().get(0).getFormatted().getValue())
-                .licenseNumber(license.getNum().get(0).getFormatted().getValue())
-                .licenseRenewStartDate(toLocalDate(license.getRenewStartDate().get(0).getFormatted()))
-                .licenseRenewEndDate(toLocalDate(license.getRenewEndDate().get(0).getFormatted()))
+                .licenseName(license.getName() != null ?license.getName().get(0).getFormatted().getValue(): null)
+                .licensePersonalNumber(license.getPersonalNum() != null ?license.getPersonalNum().get(0).getFormatted().getValue(): null)
+                .licenseType(license.getType() != null ?license.getType().get(0).getText(): null)
+                .licenseAddress(license.getAddress() != null ? license.getAddress().get(0).getFormatted().getValue(): null)
+                .licenseNumber(license.getName() != null ? license.getNum().get(0).getFormatted().getValue(): null)
+                .licenseRenewStartDate(license.getRenewStartDate() != null ? toLocalDate(license.getRenewStartDate().get(0).getFormatted()): null)
+                .licenseRenewEndDate(license.getRenewEndDate() != null ? toLocalDate(license.getRenewEndDate().get(0).getFormatted()): null)
                 .licenseCondition(license.getCondition() != null ? license.getCondition().get(0).getFormatted().getValue() : null)
-                .licenseCode(license.getCode().get(0).getFormatted().getValue())
-                .licenseIssueDate(toLocalDate(license.getIssueDate().get(0).getFormatted()))
-                .licenseAuthority(license.getAuthority().get(0).getFormatted().getValue())
+                .licenseCode(license.getCode() != null ?license.getCode().get(0).getFormatted().getValue(): null)
+                .licenseIssueDate(license.getIssueDate() != null ? toLocalDate(license.getIssueDate().get(0).getFormatted()): null)
+                .licenseAuthority(license.getAuthority() != null ? license.getAuthority().get(0).getFormatted().getValue(): null)
                 .build();
     }
 }

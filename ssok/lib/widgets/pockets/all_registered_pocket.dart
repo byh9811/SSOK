66d6c23c5ed77ad680bdf0e61ca5c7128ee5b71a
eat159357 <eat159357@gmail.com>
2023-11-13 16:41:25 +0900

    S09P31C107-885/feat: 잔돈 저금 기능 추가

--- ssok/lib/widgets/pockets/all_registered_pocket.dart
+++ ssok/lib/widgets/pockets/all_registered_pocket.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:ssok/http/http.dart';
+import 'package:ssok/http/token_manager.dart';
 import 'package:ssok/widgets/pockets/childrens/my_account.dart';
 import 'package:ssok/widgets/pockets/childrens/my_pocket.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class AllRegisteredPocket extends StatefulWidget {
   const AllRegisteredPocket({super.key, this.pocketTotalDonate, this.pocketTotalPoint, this.pocketIsChangeSaving,});
@@ -17,6 +21,92 @@ int level = 0;
 double exp = 0;
 String name = "";
 bool _isCheckedChanges = false;
+ApiService apiService = ApiService();
+
+Future<bool> editPocketIsChangeSaving() async{
+  final response = await apiService.postRequestWithoutData(
+    "pocket-service/pocket/change-saving",
+     TokenManager().accessToken);
+
+  if(response.statusCode == 200){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+void showAlet() async{
+  await showDialog(
+                barrierDismissible: false,
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text("잔돈 저금하기 변경 실패"),
+                  content: Text(
+                    "잔돈 저금하기 변경에 실패했습니다. 다시 시도해주세요.",
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                  actions: <Widget>[  
+                    TextButton(
+                      onPressed: () { 
+                        Navigator.of(context).pop();                       
+                        },
+                      child: Text("확인"),
+                    ),
+                  ],
+                ),
+              );
+}
+
+
+Future<bool> switchPocketIsChangeSaving() async{
+  bool isSwitch = false;
+
+  await showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) => AlertDialog(
+            title: Text(
+              _isCheckedChanges ? "잔돈 저금하기 취소" // 잔돈 저금하기 -> 저금 안하기 
+               : "잔돈 저금하기" // 잔돈 저금 안하기 -> 저금하기
+                    ),
+            content: Text(
+              _isCheckedChanges ? "더이상 결제 시 잔돈이 저금되지 않습니다." 
+              : "1000원 이하이의 잔돈이 포켓머니에 \n저금 됩니다.\n\n예) 1700 결제 시 300원 저금"
+              ,
+              style: TextStyle(color: Colors.black, fontSize: 16),
+            ),
+            actions: <Widget>[  
+              TextButton(
+                onPressed: () async {
+                  // Navigator.pop(context);
+                  // _isCheckedChanges = !_isCheckedChanges;
+                  isSwitch = true;
+                  
+                  if(await editPocketIsChangeSaving()){
+                    Navigator.of(context).pop();
+                  }else{
+                    isSwitch = false;
+                    Navigator.of(context).pop();
+                    showAlet();
+                  }
+                  
+                  
+                },
+                child: Text("확인 "),
+              ),
+              TextButton(
+                onPressed: () { 
+                  Navigator.of(context).pop();
+                  },
+                child: Text("취소"),
+              ),
+            ],
+          ),
+        );
+
+      return isSwitch;
+}
+
 
 @override
   void initState() {
@@ -84,11 +174,12 @@ bool _isCheckedChanges = false;
             ),
             Switch(
               value: _isCheckedChanges,
-              onChanged: (value) {
-                setState(() {
-                  
-                  _isCheckedChanges = value;
-                });
+              onChanged: (value) async {
+                if(await switchPocketIsChangeSaving()){
+                  setState(() {
+                    _isCheckedChanges = value;
+                  });                  
+                }
               },
             )]),
           ],
@@ -101,11 +192,6 @@ bool _isCheckedChanges = false;
           },
         ),
         SizedBox(height: screenHeight * 0.03),
-
-
-
-
-        
         Divider(
           height: 1,
           indent: screenWidth * 0.1,

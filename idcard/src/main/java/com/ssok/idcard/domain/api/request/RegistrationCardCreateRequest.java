4a446af70a974b5daeb84466afe702819dbd1f13
Yuhojae <ho_0214@naver.com>
2023-11-14 19:40:00 +0900

    feat : 신분증, 운전면허증 validation 구현

--- idcard/src/main/java/com/ssok/idcard/domain/api/request/RegistrationCardCreateRequest.java
+++ idcard/src/main/java/com/ssok/idcard/domain/api/request/RegistrationCardCreateRequest.java
@@ -1,6 +1,7 @@
 package com.ssok.idcard.domain.api.request;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public record RegistrationCardCreateRequest(
         String registrationCardName,
@@ -9,4 +10,44 @@ public record RegistrationCardCreateRequest(
         LocalDate registrationCardIssueDate,
         String registrationCardAuthority
 ) {
+
+    public String validate() {
+        // Validate registrationCardName
+        if (registrationCardName.matches(".*[0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*")) {
+            return "Name cannot contain digits or special characters";
+        }
+
+        // Validate registrationCardPersonalNumber
+        if (registrationCardPersonalNumber == null) {
+            return "Personal number cannot be null";
+        }
+        // Check if registrationCardPersonalNumber contains only digits and "-"
+        if (!registrationCardPersonalNumber.matches("[0-9-]+")) {
+            return "Personal number can only contain digits and '-'";
+        }
+
+        // Validate registrationCardAddress
+        if(registrationCardAddress == null){
+            return "registrationCardAddress cannot be null";
+        }
+        if (registrationCardAddress.length() > 200) {
+            return "Address length cannot exceed 200 characters";
+        }
+
+        // Validate registrationCardIssueDate
+        try {
+            String temp = registrationCardIssueDate.toString();
+            LocalDate.parse(temp);
+        } catch (DateTimeParseException e) {
+            return "Invalid issue date format. Please use the 'yyyy-MM-dd' format.";
+        }
+
+        // Validate registrationCardAuthority
+        if (registrationCardAuthority == null) {
+            return "registrationCardAuthority cannot be null";
+        }
+
+        // If all validations pass, return null (indicating success)
+        return null;
+    }
 }

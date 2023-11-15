6842faada3a5744ab68130b8a48433b6d3354eb4
marugy <gyulife7301@naver.com>
2023-11-15 14:54:54 +0900

    chore : businesscard detail i dont know

--- ssok/lib/screens/businesscard/business_card_detail_page.dart
+++ ssok/lib/screens/businesscard/business_card_detail_page.dart
@@ -57,6 +57,12 @@ class BusinessCardDetail extends StatefulWidget {
   State<BusinessCardDetail> createState() => _BusinessCardDetail(args);
 }
 
+class NameCardTotal{
+  late NameCardHead nameCardHead;
+  late NameCardBody nameCardBody;
+  late NameCardPos nameCardPos;
+}
+
 class NameCardHead {
   late String? nameCardImage;
   late String? nameCardName;
@@ -109,9 +115,6 @@ class _BusinessCardDetail extends State<BusinessCardDetail> {
     super.initState();
     getNameCardDetail();
   }
-
-
-
 
   void getNameCardDetail() async {
     print(args);
@@ -167,8 +170,7 @@ class BusinessCardDetailHeader extends StatefulWidget {
   const BusinessCardDetailHeader({super.key, required this.nameCardHead});
 
   @override
-  State<BusinessCardDetailHeader> createState() =>
-      _BusinessCardDetailHeaderState(nameCardHead);
+  State<BusinessCardDetailHeader> createState() =>_BusinessCardDetailHeaderState(nameCardHead);
 }
 
 class _BusinessCardDetailHeaderState extends State<BusinessCardDetailHeader> {
@@ -176,7 +178,9 @@ class _BusinessCardDetailHeaderState extends State<BusinessCardDetailHeader> {
   double _angle = 0;
   final NameCardHead nameCardHead;
   late String nameCardMemo = "";
+
   ApiService apiService = ApiService();
+  
   _BusinessCardDetailHeaderState(this.nameCardHead);
 
   void updateStatus() async{

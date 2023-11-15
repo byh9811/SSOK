f2e54b566bfa07a54f004f3632e57206afeb4297
eat159357 <eat159357@gmail.com>
2023-11-15 17:36:02 +0900

    S09P31C107-893/fix: 알림창 수정

--- ssok/lib/widgets/frequents/confirm.dart
+++ ssok/lib/widgets/frequents/confirm.dart
@@ -1,26 +1,91 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class Constants {
+  Constants._();
+  static const double padding = 20;
+  static const double avatarRadius = 45;
+}
+
 void confirmDialog(
     BuildContext context, String title, String content, Function() onPressed) {
   showDialog(
     context: context,
+    // transitionDuration: Duration(milliseconds: 700),
     builder: (BuildContext context) {
       return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(Constants.padding),
+        ),
+        elevation: 0,
+        // backgroundColor: Colors.transparent,
         title: Text(title),
         content: Text(content),
         actions: [
           TextButton(
             onPressed: () {
-                Navigator.of(context).pop(); // 다이얼로그 닫기
-              },
-              child: Text("취소")
+              Navigator.of(context).pop(); // 다이얼로그 닫기
+            },
+            child: Text(
+              "취소",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Color(0xFFF5F5F6)),
+            ),
           ),
           TextButton(
             onPressed: onPressed,
-            child: Text("확인"),
+            child: Text(
+              "확인",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Color(0xFF00ADEF)),
+            ),
           ),
         ],
+      );
+    },
+  );
+}
+
+void showCustomDialog(BuildContext context) {
+  showGeneralDialog(
+    context: context,
+    barrierLabel: "Barrier",
+    barrierDismissible: true,
+    barrierColor: Colors.black.withOpacity(0.5),
+    transitionDuration: Duration(milliseconds: 700),
+    pageBuilder: (_, __, ___) {
+      return Center(
+        child: Container(
+          height: 240,
+          child: SizedBox.expand(child: FlutterLogo()),
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(40)),
+        ),
+      );
+    },
+    transitionBuilder: (_, anim, __, child) {
+      Tween<Offset> tween;
+      if (anim.status == AnimationStatus.reverse) {
+        tween = Tween(begin: Offset(-1, 0), end: Offset.zero);
+      } else {
+        tween = Tween(begin: Offset(1, 0), end: Offset.zero);
+      }
+
+      return SlideTransition(
+        position: tween.animate(anim),
+        child: FadeTransition(
+          opacity: anim,
+          child: child,
+        ),
       );
     },
   );
